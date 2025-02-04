d635355da9949a661271db9cae0e47e67c90dbf8
Nikita Rogachev <nikitarogacev8@gmail.com>
2025-02-04 13:22:29 +0600

    HomeWork 28: Убрал лишний код.

--- lib/widgets/new_contact.dart
+++ lib/widgets/new_contact.dart
@@ -1,164 +1,3 @@
-// import 'dart:math';
-// import 'package:flutter/material.dart';
-// import 'package:contacts/helpers/format_datetime.dart';
-// import 'package:contacts/models/contact.dart';
-//
-// class NewContact extends StatefulWidget {
-//   final void Function(Contact newContact) onContactCreated;
-//
-//   const NewContact({super.key, required this.onContactCreated});
-//
-//   @override
-//   State<NewContact> createState() => _NewContactState();
-// }
-//
-// class _NewContactState extends State<NewContact> {
-//   var randomId = Random().nextInt(100);
-//   var name = '';
-//   var surname = '';
-//   var phone = '';
-//   var mail = '';
-//
-//   DateTime? selectedDate;
-//
-//   final dateController = TextEditingController();
-//
-//   void onCanceled() {
-//     Navigator.pop(context);
-//   }
-//
-//   void onSaved() {
-//     if (name.isEmpty) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Имя обязательно для заполнения')),
-//       );
-//       return;
-//     }
-//
-//     final newContact = Contact(
-//       id: randomId,
-//       name: name,
-//       surname: surname,
-//       phone: phone,
-//       email: mail,
-//       dateOfBirth: selectedDate,
-//     );
-//
-//     widget.onContactCreated(newContact);
-//     Navigator.pop(context);
-//   }
-//
-//   void onDateTap() async {
-//     final now = DateTime.now();
-//     final firstDate = DateTime(now.year - 100, now.month, now.day);
-//     final lastDate = now;
-//
-//     final dateFromUser = await showDatePicker(
-//       context: context,
-//       firstDate: firstDate,
-//       lastDate: lastDate,
-//       initialDate: selectedDate ?? DateTime.now(),
-//     );
-//     if (dateFromUser != null) {
-//       setState(() {
-//         selectedDate = dateFromUser;
-//         dateController.text = formatDate(dateFromUser);
-//       });
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: double.infinity,
-//       padding: EdgeInsets.all(24),
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Row(
-//             children: [
-//               Expanded(
-//                 child: TextField(
-//                   onTap: onDateTap,
-//                   readOnly: true,
-//                   controller: dateController,
-//                   decoration: InputDecoration(
-//                     label: Text('Дата рождения'),
-//                     hintText: 'Выберите дату',
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(width: 16),
-//             ],
-//           ),
-//           SizedBox(height: 16),
-//           Row(
-//             children: [
-//               Expanded(
-//                 child: TextField(
-//                   onChanged: (value) => setState(() => name = value),
-//                   decoration: InputDecoration(
-//                     label: Text('Имя'),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(width: 16),
-//               Expanded(
-//                 child: TextField(
-//                   onChanged: (value) => setState(() => surname = value),
-//                   decoration: InputDecoration(
-//                     label: Text('Фамилия'),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           Row(
-//             children: [
-//               Expanded(
-//                 child: TextField(
-//                   onChanged: (value) => setState(() => phone = value),
-//                   decoration: InputDecoration(
-//                     label: Text('Телефон'),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(width: 16),
-//               Expanded(
-//                 child: TextField(
-//                   onChanged: (value) => setState(() => mail = value),
-//                   decoration: InputDecoration(
-//                     label: Text('Почта'),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           SizedBox(height: 16),
-//           Row(
-//             children: [
-//               Expanded(
-//                 child: TextButton(
-//                   onPressed: onCanceled,
-//                   child: Text('Cancel'),
-//                 ),
-//               ),
-//               SizedBox(width: 16),
-//               Expanded(
-//                 child: ElevatedButton(
-//                   onPressed: onSaved,
-//                   child: Text('Save'),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:contacts/helpers/format_datetime.dart';
 import 'package:contacts/models/contact.dart';
@@ -173,8 +12,7 @@ class NewContact extends StatefulWidget {
 }
 
 class _NewContactState extends State<NewContact> {
-  var randomId = DateTime.now()
-      .millisecondsSinceEpoch; // Генерация уникального ID через текущее время
+  var randomId = DateTime.now().millisecondsSinceEpoch;
 
   var name = '';
   var surname = '';
@@ -198,7 +36,7 @@ class _NewContactState extends State<NewContact> {
     }
 
     final newContact = Contact(
-      id: randomId, // Уникальный ID
+      id: randomId,
       name: name,
       surname: surname,
       phone: phone,
